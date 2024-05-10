51103c1d8424701ab3fc6cd4d1e06bd8f6d13ea9
tomibrg <162723299+tomibrg@users.noreply.github.com>
2024-05-10 13:41:30 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,46 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_remove_elements) !=0 :    
+        if len(list_to_remove_elements) >= 6:
+            del list_to_remove_elements[0]
+            del list_to_remove_elements[3]
+            del list_to_remove_elements[3]
+        elif len(list_to_remove_elements) == 5:
+            del list_to_remove_elements[0]
+            del list_to_remove_elements[3]
+        elif len(list_to_remove_elements) < 5:
+            del list_to_remove_elements[0]
+        else:
+            return False
+        return list_to_remove_elements
+    elif len(list_to_remove_elements) == 0:
+        return []
+    
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    list_to_add_elements.insert(0, 'Pink')
+    list_to_add_elements.append('Yellow')
+    return list_to_add_elements
+
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_check) == 0:
+        return True
+    else:
+        return False
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(list_to_compare1)>= 3 and len(list_to_compare2) >=3:
+        if list_to_compare1[2] == list_to_compare2[2]:
+            return True
+        else:
+            return False
+    if len(list_to_compare1) < 3 or len(list_to_compare2) < 3:
+        return False
+    
+        
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    return [list_of_lists_to_modify[0][:2], list_of_lists_to_modify[1][1:4],list_of_lists_to_modify[2][-2:]]
